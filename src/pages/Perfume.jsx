01beb32d626992764f6894fb842a33f7ee511bee
redu <nara1415karol@gmail.com>
2026-06-11 20:10:04 +0300

    build: fix path casing and update missing dependencies for deployment

--- src/pages/Perfume.jsx
+++ src/pages/Perfume.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import PerfumeHero from "../components/modules/perfume/PerfumeHero";
-import FragranceFamilyFilter from "../components/modules/perfume/FragranceFamilyFilter";
-import FeaturedFragrances from "../components/modules/perfume/FeaturedFragrances";
-import PerfumeCollection from "../components/modules/perfume/PerfumeCollection";
-import SignatureScentFinder from "../components/modules/perfume/SignatureScentFinder";
-import PerfumeNotesEducation from "../components/modules/perfume/PerfumeNotesEducation.jsx";
-import WhyChooseFragrances from "../components/modules/perfume/WhyChooseFragrances.jsx";
-import PerfumeReviews from "../components/modules/perfume/PerfumeReviews.jsx";
-import PerfumeFAQ from "../components/modules/perfume/PerfumeFAQ.jsx";
+import PerfumeHero from "../components/Modules/perfume/PerfumeHero.jsx";
+import FragranceFamilyFilter from "../components/Modules/perfume/FragranceFamilyFilter.jsx";
+import FeaturedFragrances from "../components/Modules/perfume/FeaturedFragrances.jsx";
+import PerfumeCollection from "../components/Modules/perfume/PerfumeCollection.jsx";
+import SignatureScentFinder from "../components/Modules/perfume/SignatureScentFinder.jsx";
+import PerfumeNotesEducation from "../components/Modules/perfume/PerfumeNotesEducation.jsx";
+import WhyChooseFragrances from "../components/Modules/perfume/WhyChooseFragrances.jsx";
+import PerfumeReviews from "../components/Modules/perfume/PerfumeReviews.jsx";
+import PerfumeFAQ from "../components/Modules/perfume/PerfumeFAQ.jsx";
 function Perfume() {
   const [activeCategory, setActiveCategory] = useState("all");
 
