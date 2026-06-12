3c07143708c20a77d93d22a072f4bda8178946e0
redu <nara1415karol@gmail.com>
2026-06-12 16:26:13 +0300

    Remove unused CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
-import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/layout/Navbar";
 
 function App(){
   return(
-    <CartProvider>
+  
       <AppRoutes/>
-    </CartProvider>
+    
   )
 }
 
